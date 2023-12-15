15f09add098ee6aaea2dd12f98339b09c9679556
apinsdigitall <apinsdigital@gmail.com>
2023-12-15 22:33:29 +0700

    update controller dan route

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -4,7 +4,7 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 
-class ControllerAuth extends Controller
+class AuthController extends Controller
 {
     /**
      * Display a listing of the resource.
@@ -12,6 +12,11 @@ class ControllerAuth extends Controller
     public function index()
     {
         //
+    }
+
+    public function login()
+    {
+        echo "test";
     }
 
     /**
