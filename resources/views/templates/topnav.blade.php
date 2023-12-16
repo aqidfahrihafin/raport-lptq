11a37bb9628f5268d332e34c93725549efdeea32
aqidfahrihafin <aqidfahrihafin@gmail.com>
2023-12-16 09:56:08 +0700

    add topnav

--- resources/views/templates/topnav.blade.php
+++ resources/views/templates/topnav.blade.php
@@ -16,17 +16,66 @@
                               <i class="bx bx-user mr-2"></i>Profil Pesantren </a>
                       </li>
                       <li class="nav-item dropdown">
+                        <a class="nav-link dropdown-toggle arrow-none" href="#" id="topnav-dashboard"
+                            role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                            <i class="bx bx-customize mr-2"></i>Lembaga <div class="arrow-down"></div>
+                        </a>
+                        <div class="dropdown-menu" aria-labelledby="topnav-dashboard">
+
+                            <a href="#" class="dropdown-item">Pengaturan</a>
+                            <a href="#" class="dropdown-item">Mata Pelajaran</a>
+                            <a href="dashboard-crypto.html" class="dropdown-item">Kelas</a>
+                        </div>
+                    </li>
+
+                      <li class="nav-item dropdown">
                           <a class="nav-link dropdown-toggle arrow-none" href="#" id="topnav-dashboard"
                               role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-                              <i class="bx bx-customize mr-2"></i>Lembaga <div class="arrow-down"></div>
+                              <i class="bx bx-customize mr-2"></i>Guru <div class="arrow-down"></div>
                           </a>
                           <div class="dropdown-menu" aria-labelledby="topnav-dashboard">
 
-                              <a href="index.html" class="dropdown-item">Default</a>
-                              <a href="dashboard-saas.html" class="dropdown-item">Saas</a>
-                              <a href="dashboard-crypto.html" class="dropdown-item">Crypto</a>
+                              <a href="#" class="dropdown-item">Data Guru</a>
+                              <a href="#" class="dropdown-item">Mengajar</a>
                           </div>
                       </li>
+
+                      <li class="nav-item dropdown">
+                          <a class="nav-link dropdown-toggle arrow-none" href="#" id="topnav-dashboard"
+                              role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                              <i class="bx bx-customize mr-2"></i>Santri <div class="arrow-down"></div>
+                          </a>
+                          <div class="dropdown-menu" aria-labelledby="topnav-dashboard">
+
+                              <a href="#" class="dropdown-item">Data Santri</a>
+                              <a href="#" class="dropdown-item">Pindah Kelas</a>
+                              <a href="#" class="dropdown-item">Naik Kelas</a>
+                              <a href="#" class="dropdown-item">Data Alumni</a>
+                          </div>
+                      </li>
+
+                      <li class="nav-item dropdown">
+                          <a class="nav-link dropdown-toggle arrow-none" href="#" id="topnav-dashboard"
+                              role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                              <i class="bx bx-customize mr-2"></i>Raport <div class="arrow-down"></div>
+                          </a>
+                          <div class="dropdown-menu" aria-labelledby="topnav-dashboard">
+                              <a href="#" class="dropdown-item">Pengatusan Cetak</a>
+                              <a href="#" class="dropdown-item">Status Nilai</a>
+                              <a href="#" class="dropdown-item">Cetak Raport</a>
+                          </div>
+                      </li>
+                      <li class="nav-item dropdown">
+                          <a class="nav-link dropdown-toggle arrow-none" href="#" id="topnav-dashboard"
+                              role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                              <i class="bx bx-user mr-2"></i>Admin App <div class="arrow-down"></div>
+                          </a>
+                          <div class="dropdown-menu" aria-labelledby="topnav-dashboard">
+                              <a href="#" class="dropdown-item">Data Admin</a>
+                              <a href="#" class="dropdown-item">Backup & Restore</a>
+                          </div>
+                      </li>
+
 
                   </ul>
               </div>
