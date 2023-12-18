222c1b9b239f21bb17172e060932e387e285eeb3
aqidfahrihafin <aqidfahrihafin@gmail.com>
2023-12-18 08:36:46 +0700

    add view tingkat kelas

--- resources/views/admin/kelas/index.blade.php
+++ resources/views/admin/kelas/index.blade.php
@@ -18,6 +18,36 @@
                         <hr>
                     </div>
 
+                    <div class="alert alert-primary" role="alert" align="justify">
+                        <i class="mdi mdi-bullseye-arrow"></i>
+                        <b>Perhatian !</b> Halaman ini digunakan untuk menambahkan kelas pada aplikasi <b>E-Raport</b>. Silahkan pilih Sanah Dirasah dan semester sesuai dengan kelas yang ingin di tambahkan.
+                    </div>
+
+                    <div class="row">
+                        <div class="col-xl-6">
+                            <form>
+                                <div class="form-group">
+                                    <label class="control-label">Sanah Dirasah</label>
+                                    <select class="form-control">
+                                        <option value="">1445 H</option>
+                                    </select>
+                                </div>
+                            </form>
+                        </div>
+                        <div class="col-xl-6">
+                            <form>
+                                <div class="form-group">
+                                    <label class="control-label">Tingkat Kelas</label>
+                                    <select class="form-control">
+                                        <option value="">Tahfidz</option>
+                                    </select>
+                                </div>
+                            </form>
+                        </div>
+                    </div>
+
+                    <hr>
+
                     <div class="table-responsive">
 
                         <table id="datatable" class="table table-striped table-bordered dt-responsive nowrap"
@@ -25,9 +55,9 @@
                             <thead>
                                 <tr>
                                     <th width="10px">No</th>
-                                    <th>Kode Kelas</th>
-                                    <th>Nama Kelas</th>
-                                    <th>Status</th>
+                                    <th>Sanah Dirasah</th>
+                                    <th>Tingkat Kelas</th>
+                                    <th>Pembimbing</th>
                                     <th width="100px">Action</th>
                                 </tr>
                             </thead>
@@ -35,17 +65,12 @@
                             <tbody>
                                 <tr>
                                     <td>1</td>
-                                    <td>AXI</td>
-                                    <td>XI Sembilan</td>
-                                    <td align="center">
-                                        <span class="badge badge-pill badge-success font-size-8">Aktif</span>
-                                    </td>
+                                    <td>1445 H</td>
+                                    <td>Tahfidz</td>
+                                    <td>Apins Digital</td>
                                     <td align="center">
                                         <button type="button" class="btn btn-warning waves-effect waves-light btn-sm">
                                             <i class="mdi mdi-pencil"></i>
-                                        </button>
-                                        <button type="button" class="btn btn-danger waves-effect waves-light btn-sm">
-                                            <i class="mdi mdi-trash-can"></i>
                                         </button>
                                     </td>
                                 </tr>
@@ -70,20 +95,29 @@
                     </button>
                 </div>
                 <div class="modal-body">
+
+                    <div class="alert alert-primary" role="alert" align="justify">
+                        <i class="mdi mdi-bullseye-arrow"></i>
+                        <b>Perhatian !</b> pastikan isian Sanah Dirasah, Tingkat Kelas dan Pembimbing pada aplikasi <b>E-Raport</b> di isikan dengan benar.
+                    </div>
+
                     <form>
                         <div class="form-group">
-                            <label for="kode">Kode Kelas</label>
-                            <input type="text" class="form-control" id="kode">
-                        </div>
-                        <div class="form-group">
-                            <label for="tahun">Nama Kelas</label>
-                            <input type="text" class="form-control" id="tahun">
-                        </div>
-                        <div class="form-group">
-                            <label class="control-label">Status</label>
+                            <label class="control-label">Sanah Dirasah</label>
                             <select class="form-control">
-                                <option value="">Aktif</option>
-                                <option value="">Non Aktif</option>
+                                <option value="">1445 H</option>
+                            </select>
+                        </div>
+                        <div class="form-group">
+                            <label class="control-label">Tingkat Kelas</label>
+                            <select class="form-control">
+                                <option value="">Tahfidz</option>
+                            </select>
+                        </div>
+                        <div class="form-group">
+                            <label class="control-label">Pembimbing Kelas</label>
+                            <select class="form-control">
+                                <option value="">Apins Digital</option>
                             </select>
                         </div>
                         <hr>
